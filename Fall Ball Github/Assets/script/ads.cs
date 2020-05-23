8fbaa6b0b7f8e028fe3dce8b32acff9ecccd6050
nap-tech-games <naptechgames@gmail.com>
2020-05-23 13:22:38 +0600

    Prototype Uploaded

---
+++ Fall Ball Github/Assets/script/ads.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Advertisements;
+using UnityEngine;
+
+public class ads : MonoBehaviour {
+
+	public shop Shop;
+	public gameManager gm;
+	private int scene;
+
+	public void showAds(int n){
+
+		scene = n;
+
+		if (Advertisement.IsReady ()) {
+
+			Advertisement.Show ("rewardedVideo", new ShowOptions (){ resultCallback = handelAds });
+		}
+	}
+	public void showAdsSkip(){
+
+		if (Advertisement.IsReady ()) {
+
+			Advertisement.Show ("video", new ShowOptions (){ resultCallback = handelAds });
+		}
+	}
+
+	public void handelAds(ShowResult result){
+
+		switch (result) {
+
+		case ShowResult.Finished:
+			print ("ads finished");
+			if (scene == 1) {
+				Shop.watch ();
+			} else if (scene == 2) {
+				print ("ads watched");
+				gm.revive ();
+			}
+			break;
+		case ShowResult.Skipped:
+			print ("ads skipped");
+			break;
+		case ShowResult.Failed:
+			print ("ads failed");
+			break;
+		}
+
+	}
+	
+}
